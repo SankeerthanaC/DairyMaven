1db1ced2d309b0c5c2622816a7361097c77ab640
SankeerthanaC <146322883+SankeerthanaC@users.noreply.github.com>
2023-10-29 11:50:55 +0530

    Add files via upload

---
+++ src/test/java/pro/dairyproduct/DairyproductApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.dairyproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DairyproductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
